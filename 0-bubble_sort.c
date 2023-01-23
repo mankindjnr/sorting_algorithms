c8791375786a425cb62137da5e2d04c4c53b926a
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-01-23 04:21:22 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,11 +1,10 @@
 #include "sort.h"
 
 /**
- *bubble_sort - sort integers using bubble sort
- *@array: the array of integers
- *@size: the sizze of a the array
+ *swap - swaps two integrs in order of ascending
+ *@a: the first integer
+ *@b: the second integer to compare to
  */
-
 void swap(int *a, int *b)
 {
 	int temp;
@@ -15,16 +14,25 @@ void swap(int *a, int *b)
 	*b = temp;
 }
 
+/**
+ *bubble_sort - sort integers using bubble sort
+ *@array: the array of integers
+ *@size: the sizze of a the array
+ *
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int flag;
 
+	if (size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		flag = 0;
 
-		for (j = 0; j < (size - 1); j++)
+		for (j = 0; j < (size - 1 - i); j++)
 		{
 			if (array[j] > array[j + 1])
 			{
@@ -32,7 +40,7 @@ void bubble_sort(int *array, size_t size)
 				print_array(array, size);
 				flag = 1;
 			}
-        	}
+		}
 		if (flag == 0)
 			break;
 	}
