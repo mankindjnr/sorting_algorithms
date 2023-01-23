dffc780e215ddc26fd873e0564a12df6bdcc7698
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-01-23 04:08:51 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,16 +18,22 @@ void swap(int *a, int *b)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int flag;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		flag = 0;
+
+		for (j = 0; j < (size - 1 - i); j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+				flag = 1;
 			}
-			print_array(array, size);
-		}
+        	}
+		if (flag == 0)
+			break;
 	}
 }
